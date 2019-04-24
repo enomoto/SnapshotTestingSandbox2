7cc09ba59ad8ea7ab6491d3f44c978dff8e9ac2c
Kenta Enomoto <knt527@gmail.com>
2019-04-24 23:45:25 +0900

    Add snapshot test case

--- SnapshotTestingSandbox2Tests/SnapshotTestingSandbox2Tests.swift
+++ SnapshotTestingSandbox2Tests/SnapshotTestingSandbox2Tests.swift
@@ -1,34 +1,12 @@
-//
-//  SnapshotTestingSandbox2Tests.swift
-//  SnapshotTestingSandbox2Tests
-//
-//  Created by enomoto on 2019/04/24.
-//  Copyright © 2019 enomoto. All rights reserved.
-//
-
 import XCTest
+import SnapshotTesting
 @testable import SnapshotTestingSandbox2
 
 class SnapshotTestingSandbox2Tests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let vc = ViewController()
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        record = true
+        assertSnapshot(matching: vc, as: .image)
     }
-
 }
