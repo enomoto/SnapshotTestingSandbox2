4e5079dc80383aff037de74d05a5834be23f7fe0
Kenta Enomoto <knt527@gmail.com>
2019-04-24 23:27:56 +0900

    Initial commit

---
+++ SnapshotTestingSandbox2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotTestingSandbox2
+//
+//  Created by enomoto on 2019/04/24.
+//  Copyright © 2019 enomoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
